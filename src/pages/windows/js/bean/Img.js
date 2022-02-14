6fe5309c43a413eeb290332801386e895d1d463f
wangwei <vict5220@163.com>
2022-02-14 17:05:48 +0800

    迁移原生canvas

---
+++ src/pages/windows/js/bean/Img.js
@@ -0,0 +1,30 @@
+import Item from './Item.js.js'
+
+/**
+ * 图片组件
+ */
+export default class Img extends Item{
+    src=null; // 路径
+    img=null; // Image对象
+    constructor(src){
+        super();
+        this.src = src;
+    }
+
+    async setSize(width, height){
+        this.width = width;
+        this.height = height;
+        // this.x = x;
+        // this.y = y;
+
+        this.img = new Image(this.width, this.height);
+        this.img.src = this.src;
+
+        await new Promise((r)=>{
+            this.img.onload=()=>{
+                r();
+            };
+        }) 
+        
+    }
+}
